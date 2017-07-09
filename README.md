55311f9a6be5e0067f8f3f629ab0912354d12cd4
Clay Sampson <pdgeek@gmail.com>
2017-07-09 08:41:54 -0400

    prep for wider distribution

---
+++ README.md
@@ -0,0 +1,3 @@
+# Scope Marquee
+
+Control a SSD1306 OLED display on a Raspberry Pi 2 or 3.
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from distutils.core import setup
+
+_version = "0.1.0"
+
+setup(name='scope-marquee',
+      version=_version,
+      description="Python service and zeromq interface to control an SSD1306-based display on a Raspberry Pi 2/3"
+      author='Clay Sampson',
+      author_email='pdgeek@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+      license="MIT",
+      install_requires=["PIL", "zmq", "Adafruit_GPIO", "Adafruit_SSD1306"],
+      scripts=['scope-marquee/scope-marquee.py', 'scope-marquee/tell-marquee.py'],
+     )
