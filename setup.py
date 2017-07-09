e01b62924e2a1bb89db3239b3c26529ca03176c5
Clay Sampson <pdgeek@gmail.com>
2017-07-09 09:17:58 -0400

    setup includes .service file

--- setup.py
+++ setup.py
@@ -5,11 +5,22 @@ _version = "0.1.0"
 
 setup(name='scope-marquee',
       version=_version,
-      description="Python service and zeromq interface to control an SSD1306-based display on a Raspberry Pi 2/3",
+      description="Python service and zeromq interface to control an SSD1306-based display on Raspberry Pi 2/3 SPI bus",
       author='Clay Sampson',
       author_email='pdgeek@gmail.com',
       url='https://www.python.org/sigs/distutils-sig/',
       license="MIT",
-      requires=["PIL", "zmq", "Adafruit_GPIO", "Adafruit_SSD1306"],
-      scripts=['scope-marquee/scope-marquee.py', 'scope-marquee/tell-marquee.py'],
+      requires=[
+        "PIL",
+        "zmq",
+        "Adafruit_GPIO",
+        "Adafruit_SSD1306"
+        ],
+      scripts=[
+        'scope-marquee/scope-marquee.py',
+        'scope-marquee/tell-marquee.py'
+        ],
+      data_files=[
+        ('/etc/systemd/system', ['scope-marquee/scope-marquee.service'])
+        ],
      )
